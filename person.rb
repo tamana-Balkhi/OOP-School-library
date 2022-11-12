b007f9dc3825a52b81a5e781a5134462b899321f
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-11-12 08:08:09 +0430

    creat many to may relationship

--- person.rb
+++ person.rb
@@ -2,8 +2,9 @@ require_relative './nameable'
 require_relative './capitalize'
 require_relative './trimmer'
 
+
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -12,6 +13,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -22,12 +24,17 @@ class Person < Nameable
     @correctable_name = name
   end
 
+  def add_rentals(date, book_info)
+    Rental.new(date, book_info, self)
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
 
 p person = Person.new(22, 'maximilianus')
 p person.correct_name
