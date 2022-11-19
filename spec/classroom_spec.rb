b07b8f699a5d10968bc7b7a6b4d63e6b6b92df90
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-11-19 08:39:14 +0430

    test classes

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,9 +1,20 @@
-require_relative '../book'
 require_relative '../classroom'
-require_relative '../teacher'
-require_relative '../student'
-require_relative '../rental'
 
-describe Classroom
+describe 'Classroom' do
+  before :each do
+    @label = 224
+    @students = []
+  end
 
+  classroom = Classroom.new(@label)
+  context 'should create an instance of Classroom class' do
+    it 'Classroom object' do
+      expect(classroom.class) == Classroom
+    end
+  end
+  context 'should create an students list' do
+    it 'Classroom list' do
+      expect(Classroom.instance_methods(false).include?(:add_student)).to eq true
+    end
+  end
 end
